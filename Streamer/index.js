1b395882d15d2f979450c2984713f0c21e0e9628
Justin <jkwok0714@gmail.com>
2019-11-10 10:30:49 -0800

    Basic setup for streamer project

---
+++ Streamer/index.js
@@ -0,0 +1,34 @@
+const loadResult = require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+const {
+    PORT
+} = require('./constants');
+
+if (loadResult.error) {
+    Logger.error(`Error with loading config: ${loadResult.error.message ? loadResult.error.message : loadResult.error}`);
+}
+
+const staticPath = path.resolve(__dirname + '/public');
+
+/** Ensure the save directories exist so we don't error when writing */
+if (!!process.env.SAVE_PATH && !fs.existsSync(process.env.SAVE_PATH)) {
+    fs.mkdirSync(process.env.SAVE_PATH);
+}
+
+/* Configure Middleware */
+app.use(express.static(staticPath));
+
+app.use((req, res, next) => {
+    console.log(`[index] ${req.method} to ${req.originalUrl}`);
+    next();
+});
+
+app.get('/test', (req, res) => {
+    res.send('こんにちは, 「ZA WARUDO」!')
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
