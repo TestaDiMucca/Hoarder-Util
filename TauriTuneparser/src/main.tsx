5ff1b74b2a4c50325cbaa7f2f2c57b01e94c8aa7
Lazy Imposter <jkwok0714@gmail.com>
2023-12-26 09:10:24 -0800

    Fix various build errors

--- TauriTuneparser/src/main.tsx
+++ TauriTuneparser/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider, ColorModeProvider} from '@chakra-ui/react'
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeProvider>
+      <App />
+      </ColorModeProvider>
+    </ChakraProvider>
+    
   </React.StrictMode>,
 )
