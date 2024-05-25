8ef4a0604e42934670179aca21265432e8e90719
Lazy Imposter <jkwok0714@gmail.com>
2024-05-24 22:47:20 -0700

    Expand helpers for reference

--- HoarderUtil/src/util/promises.ts
+++ HoarderUtil/src/util/promises.ts
@@ -22,6 +22,21 @@ const map = async <T, R>(
   return results;
 };
 
+const mapSeries = async <T, R>(
+  items: readonly T[],
+  callback: (item: T, index: number) => Resolvable<R>
+) => {
+  const results: R[] = [];
+  let idx = 0;
+
+  for (const item of items) {
+    results.push(await callback(item, idx));
+    idx++;
+  }
+
+  return results;
+};
+
 const reduce = async <T, R>(
   items: readonly T[],
   callback: (out: R, item: T, i?: number) => Resolvable<R>,
@@ -38,13 +53,58 @@ const reduce = async <T, R>(
   return out;
 };
 
+const each = async <T>(
+  items: readonly T[],
+  callback: (item: T) => Resolvable<unknown>,
+  options?: { concurrency?: number }
+) => {
+  if (!options?.concurrency || options.concurrency <= 0) {
+    for (const item of items) {
+      await callback(item);
+    }
+
+    return;
+  }
+
+  /* Handle concurrency */
+  const itemsWithIndex = items.map((v) => ({ v }));
+  const batches = chunkArray(itemsWithIndex, options.concurrency);
+
+  for (const batch of batches) {
+    await Promise.all(batch.map(({ v }) => callback(v)));
+  }
+};
+
+const filter = async <T>(
+  items: readonly T[],
+  callback: (item: T, index: number) => Resolvable<boolean>
+) => {
+  const results: T[] = [];
+  let idx = 0;
+
+  for (const item of items) {
+    const shouldKeep = await callback(item, idx);
+
+    if (shouldKeep) {
+      results.push(item);
+    }
+
+    idx++;
+  }
+
+  return results;
+};
+
 /**
  * Stand-in for Bluebird[method]
  * courtesy of Mr. Allister Craig Smith
  */
 const promises = {
   map,
+  mapSeries,
   reduce,
+  each,
+  filter,
 };
 
 export default promises;
