071bb18deee79b91f6c9e8c8c328afbaaf7c93db
Lazy Imposter <jkwok0714@gmail.com>
2024-05-25 21:58:51 -0700

    Fix usage of 12h syntax

--- HoarderUtil/src/util/dateUtils.ts
+++ HoarderUtil/src/util/dateUtils.ts
@@ -1,11 +1,11 @@
 import * as dateFormat from 'dateformat';
 import output from './output';
 /**
- * Formats to: 'YY-MM-DD-hh-mm'
+ * Formats to: 'YY-MM-DD-HH-mm'
  *
  * @todo create options
  */
-export const formatDateString = (date: Date, mask = 'yy-mm-dd-hh-MM_') => {
+export const formatDateString = (date: Date, mask = 'yy-mm-dd-HH-MM_') => {
   try {
     return dateFormat(date, mask);
   } catch (e: any) {
