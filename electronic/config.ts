18b9a172a09efd66a868cdf8884338ee918b32e0
Lazy Imposter <jkwok0714@gmail.com>
2024-07-19 22:43:46 -0700

    Revert "Config electrons"
    This reverts commit ed65762061b2ffd48ca49fcf6c08cdfded6c8068.

--- electronic/config.ts
+++
@@ -1,1 +0,0 @@
-export const isDev = process.env.APP_IS_DEV ? true : false;
--- electronic/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('browserWindow', {
-  versions: () => ipcRenderer.invoke('versions'),
-});
