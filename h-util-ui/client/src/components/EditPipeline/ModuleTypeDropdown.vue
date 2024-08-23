16fce746628f06ca2855d93b2d153f63aebe93ff
Lazy Imposter <jkwok0714@gmail.com>
2024-08-23 11:20:02 -0700

    Re-usable dropdown menu component

--- h-util-ui/client/src/components/EditPipeline/ModuleTypeDropdown.vue
+++ h-util-ui/client/src/components/EditPipeline/ModuleTypeDropdown.vue
@@ -1,73 +1,85 @@
 <script setup lang="ts">
-import MenuRight from 'vue-material-design-icons/MenuRight.vue';
 import { ProcessingModuleType } from '@shared/common.types';
+import DropdownMenu, { MenuItem } from '../common/DropdownMenu.vue';
 
 interface Props {
   handleModuleTypeSelect: (moduleType: ProcessingModuleType) => void;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const handleModuleTypeSelect = (type: ProcessingModuleType) => () => props.handleModuleTypeSelect(type);
+
+const menuItems: MenuItem[] = [
+  {
+    type: 'item',
+    label: 'Place in directory',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.subfolder)
+  },
+  {
+    type: 'separator'
+  },
+  {
+    type: 'item',
+    label: 'Metadata tag',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.metadata)
+  },
+  {
+    type: 'item',
+    label: 'Date prefix',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.datePrefix)
+  },
+  {
+    type: 'item',
+    label: 'Dynamic renaming',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.dynamicRename)
+  },
+  { type: 'separator' },
+  {
+    type: 'item',
+    label: 'Iterate',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.iterate)
+  },
+  {
+    type: 'item',
+    label: 'Log results',
+    onClick: handleModuleTypeSelect(ProcessingModuleType.report)
+  },
+  {
+    type: 'group',
+    label: 'Media compression',
+    items: [
+      {
+        type: 'item',
+        label: 'Compress image',
+        onClick: handleModuleTypeSelect(ProcessingModuleType.compressImage)
+      },
+      {
+        type: 'item',
+        label: 'Compress video',
+        onClick: handleModuleTypeSelect(ProcessingModuleType.compressVideo)
+      },
+    ]
+  },
+  {
+    type: 'group',
+    label: 'Filtering',
+    items: [
+      {
+        type: 'item',
+        label: 'Filter file',
+        onClick: handleModuleTypeSelect(ProcessingModuleType.filter)
+      },
+      {
+        type: 'item',
+        label: 'Search image for test',
+        onClick: handleModuleTypeSelect(ProcessingModuleType.ocr)
+      },
+    ]
+  }
+]
 </script>
 
 <template>
-  <q-menu>
-    <q-list style="min-width: 100px">
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.subfolder)" clickable v-close-popup="true">
-        <q-item-section>Place in directory</q-item-section>
-      </q-item>
-
-      <q-separator />
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.metadata)" clickable v-close-popup="true">
-        <q-item-section>Metadata tag</q-item-section>
-      </q-item>
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.datePrefix)" clickable v-close-popup="true">
-        <q-item-section>Date prefix</q-item-section>
-      </q-item>
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.dynamicRename)" clickable v-close-popup="true">
-        <q-item-section>Dynamic renaming</q-item-section>
-      </q-item>
-      <q-separator />
-
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.iterate)" clickable v-close-popup="true">
-        <q-item-section>Iterate</q-item-section>
-      </q-item>
-      <q-item @click="handleModuleTypeSelect(ProcessingModuleType.report)" clickable v-close-popup="true">
-        <q-item-section>Log results</q-item-section>
-      </q-item>
-      <q-separator />
-      <q-item clickable>
-        <q-item-section>Media compression</q-item-section>
-        <q-item-section side>
-          <MenuRight :size="20" />
-        </q-item-section>
-        <q-menu anchor="top end" self="top start">
-          <q-list>
-            <q-item @click="handleModuleTypeSelect(ProcessingModuleType.compressImage)" clickable v-close-popup="true">
-              <q-item-section>Compress image</q-item-section>
-            </q-item>
-            <q-item @click="handleModuleTypeSelect(ProcessingModuleType.compressVideo)" clickable v-close-popup="true">
-              <q-item-section>Compress video</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-item>
-      <q-item clickable>
-        <q-item-section>Filtering</q-item-section>
-        <q-item-section side>
-          <MenuRight :size="20" />
-        </q-item-section>
-
-        <q-menu anchor="top end" self="top start">
-          <q-list>
-            <q-item @click="handleModuleTypeSelect(ProcessingModuleType.filter)" clickable v-close-popup="true">
-              <q-item-section>Filter file</q-item-section>
-            </q-item>
-            <q-item @click="handleModuleTypeSelect(ProcessingModuleType.ocr)" clickable v-close-popup="true">
-              <q-item-section>Search image for text</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-item>
-    </q-list>
-  </q-menu>
+  <DropdownMenu :menuItems="menuItems" />
 </template>
