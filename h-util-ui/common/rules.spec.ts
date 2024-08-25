103532d0cb5dd5828e9476e9308f454e26a84334
Lazy Imposter <jkwok0714@gmail.com>
2024-08-25 16:27:25 -0700

    Add more tests and handling scenario

--- h-util-ui/common/rules.spec.ts
+++ h-util-ui/common/rules.spec.ts
@@ -8,7 +8,9 @@ type RuleTestScenario<T extends string = string> = {
 };
 
 /** Literally just to enforce types, doesn't do anything so special */
-const createRuleTestScenario = <T extends string>(scenarios: RuleTestScenario<T>[]): RuleTestScenario<T>[] => scenarios;
+const createRuleTestScenario = <T extends string>(scenarios: RuleTestScenario<T>[]): RuleTestScenario<T>[] => {
+    return scenarios;
+};
 
 describe('le rules system', () => {
     test.each(
@@ -39,8 +41,130 @@ describe('le rules system', () => {
                     name: 'homework.mp4',
                 },
             },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.string,
+                    operator: Operator.ne,
+                    attribute: 'name',
+                    value: 'nero.png',
+                },
+                data: {
+                    name: 'nero.png',
+                },
+                result: false,
+            },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.string,
+                    operator: Operator.ne,
+                    attribute: 'name',
+                    value: 'nero.png',
+                },
+                data: {
+                    name: 'ishtar.png',
+                },
+                result: true,
+            },
         ]),
     )('Simple string matches', (scenario) => {
+        const result = evaluateRule(scenario.rule, scenario.data);
+
+        expect(result).toEqual(scenario.result);
+    });
+
+    test.each(
+        createRuleTestScenario([
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.number,
+                    operator: Operator.lt,
+                    attribute: 'fileSize',
+                    value: '2000',
+                },
+                result: true,
+                data: {
+                    fileSize: '1000',
+                },
+            },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.number,
+                    operator: Operator.lt,
+                    attribute: 'fileSize',
+                    value: '500',
+                },
+                result: false,
+                data: {
+                    fileSize: '1000',
+                },
+            },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.number,
+                    operator: Operator.gt,
+                    attribute: 'fileSize',
+                    value: '500',
+                },
+                result: true,
+                data: {
+                    fileSize: '1000',
+                },
+            },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.number,
+                    operator: Operator.gt,
+                    attribute: 'fileSize',
+                    value: '2000',
+                },
+                result: false,
+                data: {
+                    fileSize: '1000',
+                },
+            },
+        ]),
+    )('Number matches', (scenario) => {
+        const result = evaluateRule(scenario.rule, scenario.data);
+
+        expect(result).toEqual(scenario.result);
+    });
+
+    test.each(
+        createRuleTestScenario([
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.date,
+                    operator: Operator.lt,
+                    attribute: 'created',
+                    value: '2024-04-01',
+                },
+                result: false,
+                data: {
+                    created: '2024-05-01',
+                },
+            },
+            {
+                rule: {
+                    type: 'basic',
+                    attributeType: AttributeType.date,
+                    operator: Operator.lt,
+                    attribute: 'created',
+                    value: '2024-06-01',
+                },
+                result: true,
+                data: {
+                    created: '2024-05-01',
+                },
+            },
+        ]),
+    )('Date rules', (scenario) => {
         const result = evaluateRule(scenario.rule, scenario.data);
 
         expect(result).toEqual(scenario.result);
--- h-util-ui/common/rules.utils.ts
+++ h-util-ui/common/rules.utils.ts
@@ -1,4 +1,4 @@
-import { BasicRule, Rule } from './rules.types';
+import { AttributeType, BasicRule, Rule } from './rules.types';
 
 export const evaluateRule = (rule: Rule, data: Record<string, string>): boolean => {
     if (rule.type === 'basic') return evaluateBasicRule(rule, data);
@@ -16,24 +16,48 @@ export function evaluateBasicRule<T extends string = string>(
     rule: BasicRule<T>,
     data: Record<string, string>,
 ): boolean {
-    const value = data[rule.attribute];
-    switch (rule.operator) {
-        case '=':
-            return value === rule.value;
-        case '>':
-            return value > rule.value;
-        case '<':
-            return value < rule.value;
-        case '>=':
-            return value >= rule.value;
-        case '<=':
-            return value <= rule.value;
-        case '!=':
-            return value !== rule.value;
-        case 'contains':
-            return value.includes(rule.value);
-        // Add more operators as needed
-        default:
-            return false;
+    try {
+        const uncastValue = data[rule.attribute];
+
+        if (!uncastValue && !!rule.value) return false;
+
+        const attrType = rule.attributeType;
+
+        const value =
+            attrType === AttributeType.number
+                ? +uncastValue
+                : attrType === AttributeType.date
+                  ? new Date(uncastValue)
+                  : uncastValue;
+        const castRuleValue =
+            attrType === AttributeType.number
+                ? +rule.value
+                : attrType === AttributeType.date
+                  ? new Date(rule.value)
+                  : rule.value;
+
+        switch (rule.operator) {
+            case '=':
+                return value === castRuleValue;
+            case '>':
+                return value > castRuleValue;
+            case '<':
+                return value < castRuleValue;
+            case '>=':
+                return value >= castRuleValue;
+            case '<=':
+                return value <= castRuleValue;
+            case '!=':
+                return value !== castRuleValue;
+            case 'contains':
+                if (typeof value !== 'string') return false;
+
+                return value.includes(rule.value);
+            // Add more operators as needed
+            default:
+                return false;
+        }
+    } catch {
+        return false;
     }
 }
