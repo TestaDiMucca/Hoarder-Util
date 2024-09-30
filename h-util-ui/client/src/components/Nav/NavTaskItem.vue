124324c43348f59bd022faeb78123ffbf5c6859d
Lazy Imposter <jkwok0714@gmail.com>
2024-09-30 01:19:45 -0700

    Support sub-progress spinner

--- h-util-ui/client/src/components/Nav/NavTaskItem.vue
+++ h-util-ui/client/src/components/Nav/NavTaskItem.vue
@@ -14,9 +14,13 @@ const letter = computed(() => props.task.pipelineName[0] ?? '?')
 <template>
   <q-item class="container">
     <q-item-section :class="{ done: task.progress === 100 }" avatar>
-      <q-circular-progress show-value :value="task.progress" size="24px" :thickness="0.5" track-color="grey-8">
-        <span>{{ letter }}</span>
-      </q-circular-progress>
+      <div class="spinners-container">
+        <q-circular-progress show-value :value="task.progress" size="24px" :thickness="0.5" track-color="grey-8">
+          <span>{{ letter }}</span>
+        </q-circular-progress>
+        <q-circular-progress v-if="!!task.subProgress" show-value :value="task.subProgress ?? 0" size="12px"
+          :thickness="0.8" track-color="grey-8" class="mini-spinner" />
+      </div>
     </q-item-section>
     <q-item-section class="task-info">
       <div class="pipeline-name"> {{ task.pipelineName }}</div>
@@ -30,6 +34,17 @@ const letter = computed(() => props.task.pipelineName[0] ?? '?')
   display: flex;
   flex-direction: column;
   width: 80%;
+}
+
+.spinners-container {
+  width: min-content;
+  position: relative;
+}
+
+.mini-spinner {
+  position: absolute;
+  bottom: 0;
+  right: 0;
 }
 
 .done {
