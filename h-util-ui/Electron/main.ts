158f9a9dd08b2b4d563a9f75feb32583f3606bcf
Lazy Imposter <jkwok0714@gmail.com>
2024-11-07 22:31:24 -0800

    Fix ffmpeg built, quit when close

--- h-util-ui/Electron/main.ts
+++ h-util-ui/Electron/main.ts
@@ -89,14 +89,9 @@ app.whenReady().then(async () => {
     });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     output.log('All windows closed');
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
 process.on('uncaughtException', (error) => {
